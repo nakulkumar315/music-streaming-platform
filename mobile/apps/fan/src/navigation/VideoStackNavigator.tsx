b8030f8356ebb4ee3e41e5340da413d6c1997104
jasiq-labs-admin <jasiqlabs@gmail.com>
2026-09-13 12:25:11 +0530

    Phase 06: enforce authoritative video artist route state

--- mobile/apps/fan/src/navigation/VideoStackNavigator.tsx
+++ mobile/apps/fan/src/navigation/VideoStackNavigator.tsx
@@ -2,18 +2,16 @@ import React, {} from 'react';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import ArtistScreen from '../screens/ArtistScreen';
+import AuthoritativeArtistScreen from './AuthoritativeArtistScreen';
 import ArtistSubscriptionScreen from '../screens/ArtistSubscriptionScreen';
 import ContentPlayerScreen from '../screens/ContentPlayerScreen';
 import SubscriptionFlowScreen from '../screens/SubscriptionFlowScreen';
 import VideoScreen from '../screens/VideoScreen';
 
-
 export type VideoStackParamList = {
   VideoIndex: undefined;
   Artist: {
     artistId?: string;
-    unlocked?: boolean;
     contentId?: string;
   };
   ArtistSubscription: {
@@ -44,7 +42,7 @@ export default function VideoStackNavigator() {
   return (
     <Stack.Navigator id="fan-video" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="VideoIndex" component={VideoScreen} />
-      <Stack.Screen name="Artist" component={ArtistScreen} options={{ animation: 'slide_from_right' }} />
+      <Stack.Screen name="Artist" component={AuthoritativeArtistScreen} options={{ animation: 'slide_from_right' }} />
       <Stack.Screen name="ArtistSubscription" component={ArtistSubscriptionScreen} />
       <Stack.Screen name="ContentPlayer" component={ContentPlayerScreen} />
       <Stack.Screen name="SubscriptionFlow" component={SubscriptionFlowScreen} />
